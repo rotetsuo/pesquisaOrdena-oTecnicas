0a58e2e7d121fb95d4b1f9ea9092aca59775582b
rotetsuo <rotetsuo32@gmail.com>
2021-09-09 19:54:48 -0400

    Exercicio 1 - aula 2

--- aula02/Ex01.java
+++ aula02/Ex01.java
@@ -1,5 +1,25 @@
+/**
+ * • Desenvolva um programa que calcule o fatorial de um número inteiro
+ *   não negativo fornecido pelo usuário.
+ * 
+ * 
+ */
+import java.util.Scanner;
 public class Ex01{
     public static void main(String[] args) {
-        System.out.println("ola mundo");
+        Scanner entrada = new Scanner(System.in);
+            System.out.println("Digite um número inteiro não negativo:");
+            int n1 = entrada.nextInt();
+            int resultado = fatorial(n1);
+            System.out.println(resultado);
+        entrada.close();
+    }
+
+    public static int fatorial(int n1){
+        if(n1 == 0){
+            return 1;
+        }else{
+            return n1 * fatorial(n1-1); 
+        }
     }
 }
