471725ae6df5cc2a8a289bc6509ff20a98f94031
rotetsuo <rotetsuo32@gmail.com>
2021-09-26 11:14:32 -0400

    ex 2

---
+++ n1/Ex02.java
@@ -0,0 +1,16 @@
+
+public class Ex02 {
+    public static void main(String[] args) {
+        int n = 7;
+        int k = 3;
+        System.out.println(alg5(n, k));
+    }
+
+    public static int alg5(int n1, int n2){
+        if(n1 == 0){
+            return 0;
+        }else{
+            return alg5(n1-1, n2) + n2;
+        }
+    }
+}
